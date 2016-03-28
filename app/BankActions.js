7717546811f46456d2584f7a030d5b8759e5e635
xnie <xin@slimpay.com>
2016-03-28 23:38:05 +0200

    Add Flux artchitecture

---
+++ app/BankActions.js
@@ -0,0 +1,28 @@
+import AppDispatcher from './AppDispatcher';
+import bankConstants from './constants';
+
+let BankActions = {
+  createAccount() {
+    AppDispatcher.dispatch({
+      type: bankConstants.CREATED_ACCOUNT,
+      amount: 0
+    });
+  },
+
+  depositIntoAccount(amount) {
+    AppDispatcher.dispatch({
+      type: bankConstants.DEPOSITED_INTO_ACCOUNT,
+      amount: amount
+    });
+  },
+
+  withdrawFromAccount(amount) {
+    AppDispatcher.dispatch({
+      type: bankConstants.WITHDRAW_FROM_ACCOUNT,
+      amount: amount
+    });
+  },
+};
+
+
+export default BankActions;
---
+++ app/BankBalanceStore.js
@@ -0,0 +1,29 @@
+import AppDispatcher from './AppDispatcher';
+import bankConstants from './constants';
+import { ReduceStore } from 'flux/utils';
+
+let balance = 0;
+
+class BankBalanceStore extends ReduceStore {
+  getInitialState() {
+    return 0;
+  }
+
+  reduce(state, action) {
+    switch (action.type) {
+      case bankConstants.CREATED_ACCOUNT:
+        return 0;
+
+      case bankConstants.DEPOSITED_INTO_ACCOUNT:
+        return state + action.amount;
+
+      case bankConstants.WITHDRAW_FROM_ACCOUNT:
+        return state - action.amount;
+
+      default:
+        return state;
+    }
+  }
+}
+
+export default new BankBalanceStore(AppDispatcher);
